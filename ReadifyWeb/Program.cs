7129d75beb95e0ebe46eecc792e4ea7685227155
Yousef Eid <yeid53646@gmail.com>
2025-09-24 18:57:35 +0300

    Install Packages and create database

--- ReadifyWeb/Program.cs
+++ ReadifyWeb/Program.cs
@@ -1,28 +1,37 @@
+using Microsoft.EntityFrameworkCore;
+using ReadifyWeb.Data;
 
-    var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
-    // Add services to the container.
-    builder.Services.AddControllersWithViews();
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(option=>
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-    var app = builder.Build();
 
-    // Configure the HTTP request pipeline.
-    if (!app.Environment.IsDevelopment())
-    {
-        app.UseExceptionHandler("/Home/Error");
-        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        app.UseHsts();
-    }
+// Add services to the container.
+builder.Services.AddControllersWithViews();
 
-    app.UseHttpsRedirection();
-    app.UseStaticFiles();
+var app = builder.Build();
 
-    app.UseRouting();
 
-    app.UseAuthorization();
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
 
-    app.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Home}/{action=Index}/{id?}");
+app.UseHttpsRedirection();
+app.UseStaticFiles();
 
-    app.Run();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
