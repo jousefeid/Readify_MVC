382ce7ea7f387b485200ce692c5e860efaccb1d1
Yousef Eid <yeid53646@gmail.com>
2025-11-27 18:36:46 +0200

    Add , Update and Delete Action
    All CRUD  Ended

--- ReadifyWeb/Controllers/CategoryController.cs
+++ ReadifyWeb/Controllers/CategoryController.cs
@@ -18,11 +18,14 @@ namespace ReadifyWeb.Controllers
             List<Category> ObjCategoryList = _db.Categories.ToList();
             return View(ObjCategoryList);
         }
+
         public IActionResult Create()
         {
             return View();
         }
+
         [HttpPost]
+
         public IActionResult Create(Category obj)
         {
             if (obj.Name == obj.DisplayOrder.ToString())
@@ -33,13 +36,63 @@ namespace ReadifyWeb.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                return RedirectToAction("Index");
             }
             return View();
         }
-        
-        
 
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
 
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+            
+        }
 
     }
 }
