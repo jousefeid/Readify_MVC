b3bbb7fdfc460d19aff4318c415ae353ef415d55
Yousef Eid <yeid53646@gmail.com>
2025-12-01 16:17:09 +0200

    Confirm (created - updated - deleted) successfully message

--- ReadifyWeb/Controllers/CategoryController.cs
+++ ReadifyWeb/Controllers/CategoryController.cs
@@ -36,6 +36,7 @@ namespace ReadifyWeb.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
             return View();
@@ -61,6 +62,7 @@ namespace ReadifyWeb.Controllers
             {
                 _db.Categories.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
                 return RedirectToAction("Index");
             }
             return View();
@@ -90,6 +92,7 @@ namespace ReadifyWeb.Controllers
             }
             _db.Categories.Remove(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
             return RedirectToAction("Index");
             
         }
